6c57e6bfe59d409cd111c6c5ea60e865b65adecd
jonatasmcampos <camposjonatas3@gmail.com>
2022-04-22 10:06:24 -0300

    siedbar separada

--- resources/views/usuarioAdmin/home.blade.php
+++ resources/views/usuarioAdmin/home.blade.php
@@ -4,27 +4,7 @@
 
     <div class="container-externo">
 
-         {{-- PARTE DA SIEDBAR COM ICONES --}}
-        <div class="container-siedbar-icons">
-            <div class="box-menu">
-                <i class="bi bi-list d-flex justify-content-center"></i>
-            </div>
-            <div class="box-opcoes-icones">
-                <i class="bi bi-house-fill"></i>
-            </div>
-        </div>
-
-        {{-- PARTE DA SIEDBAR --}}
-        <div class="container-siedbar">
-            {{-- NOME DA EMPRESA --}}
-            <div class="box-empresa">
-                <a href="#" class="d-flex justify-content-center" style="letter-spacing: 3px;">Empresa</a>
-            </div>
-            {{-- AS OPCOES DO ADMIN --}}
-            <div class="box-opcoes">
-                <a href="#">Dashboarda</a>
-            </div>
-        </div>
+        @include('siedbar.siedbar')
     
         {{-- PARTE DO CONTEUDO --}}
         <div class="container-conteudo">
