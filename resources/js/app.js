0c3ab9c16a99d704b98bc04b86b80826d6e516dc
josehs20 <josehenriquesimiquelsouza@gmail.com>
2022-04-23 14:30:27 -0300

    atualização e criptografia de senha concluido

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 require('./bootstrap');
+
+//torna objeyo Swal como global 
+window.Swal = require('sweetalert2')
---
+++ resources/views/usuarioAdmin/minhaConta/inc/_menus.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <a href="{{ route('mihaConta.index') }}">voltar</a><br>
+    <a href="{{ route('edit_configuracao') }}">configurações</a><br>
+    <a href="{{ route('edit_senha') }}">Alterar Senha</a>
+
+</div>
