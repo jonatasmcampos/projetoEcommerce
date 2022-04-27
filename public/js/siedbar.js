dbd0e8dea3ed7bea250a14190257714088ebcfa1
jonatasmcampos <camposjonatas3@gmail.com>
2022-04-27 17:13:04 -0300

    tooltip siedbar adicionado

--- public/js/siedbar.js
+++
@@ -1,6 +0,0 @@
-var btnMenu = document.getElementById('btn-menu')
-var siedbarNomes = document.getElementById('siedbar-nomes')
-
-btnMenu.addEventListener("click", function(){
-    siedbarNomes.classList.toggle('voltar')
-})
--- resources/views/siedbar/siedbar.blade.php
+++ resources/views/siedbar/siedbar.blade.php
@@ -1,19 +1,63 @@
-{{-- PARTE DA SIEDBAR COM ICONES --}}
-<div class="container-siedbar-icons">
+{{-- SIEDABAR --}}
+<aside class="container-siedbarr" style="width: 70px; height: 100vh; background-color: #232334">
+
+    {{-- ICONE DE MENU --}}
+    <div style="
+    border: 1px solid red;
+    display: flex;
+    justify-content:center;
+    height: 72px;
+    align-items:center">
+        <i class="bi bi-list"></i>
+    </div>
+
+    <br>
+
+    {{-- DIV DOS ICONES --}}
+    <div style="
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items:center
+    ">
+
+
+        <div class="Dica">
+            <i class="bi-file-earmark-plus"></i>
+            <div class="DicaTexto">Cadastro</div>
+        </div>
+
+        <div class="Dica">
+            <i class="bi-file-earmark-plus"></i>
+            <div class="DicaTexto">Cadastro</div>
+        </div>
+
+      
+        
+
+
+    </div>
+
+
+
+
+</aside>
+
+{{-- <div class="container-siedbar-icons">
     <div id="siedbar-icones" class="box-menu">
-        {{-- <div class="box-i"> --}}
+        
         <i id="btn-menu" class="bi bi-list d-flex justify-content-center box-i"></i>
-        {{-- </div> --}}
+        
     </div>
     <div class="box-opcoes-icones">
-        {{-- DROPDOWND DE CADASTROS --}}
+        
         <div class="dropend">
             <button type="button" class="btn dropdown" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="bi bi-file-earmark-plus box-i"></i>
             </button>
             <ul class="dropdown-menu">
                 <li class="dropdown-item"><a href="{{ route('produto.index') }}">Produtos</a></li>
-                {{-- <li class="dropdown-item"><a href="{{ route('produto.create') }}">Cadastrar produto</a></li> --}}
+            
                 <li class="dropdown-item"><a href="{{ route('estoque.index') }}">Estoque</a></li>
                 <li class="dropdown-item"><a href="{{ route('categoria.create') }}">Categorias</a></li>
             </ul>
@@ -21,23 +65,22 @@
     </div>
 </div>
 
-{{-- PARTE DA SIEDBAR --}}
+
 <div id="siedbar-nomes" class="container-siedbar">
-    {{-- NOME DA EMPRESA --}}
+    
     <div class="box-empresa">
         <a href="#" class="d-flex justify-content-center" style="letter-spacing: 3px;">Empresa</a>
     </div>
-    {{-- AS OPCOES DO ADMIN --}}
+    
     <div class="box-opcoes">
 
-        {{-- DROPDOWND DE CADASTROS --}}
+        
         <div class="dropend box-individual">
             <a type="button" class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                 Cadastro
             </a>
             <ul class="dropdown-menu">
                 <li class="dropdown-item"><a href="{{ route('produto.index') }}">Produtos</a></li>
-                {{-- <li class="dropdown-item"><a href="{{ route('produto.create') }}">Cadastrar produto</a></li> --}}
                 <li class="dropdown-item"><a href="{{ route('estoque.index') }}">Estoque</a></li>
                 <li class="dropdown-item"><a href="{{ route('categoria.create') }}">Categorias</a></li>
             </ul>
@@ -46,4 +89,4 @@
 
 
     </div>
-</div>
+</div> --}}
