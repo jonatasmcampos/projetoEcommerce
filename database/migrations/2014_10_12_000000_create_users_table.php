feb9a89686cd560e5650be2783fa8ae7d7b67540
josehs20 <josehenriquesimiquelsouza@gmail.com>
2022-04-28 10:01:09 -0300

    migration e db:Seed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,10 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string("perfil")->nullable();
-            $table->string("cpf", 11)->unique()->nullable();
-            $table->string("telefone")->nullable();
-            $table->string("foto", 100)->nullable(); //foto pode ser nulo
             $table->rememberToken();
             $table->timestamps();
         });
