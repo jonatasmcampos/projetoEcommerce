d0b63d268932cd9883d2c91642d1f765c1542c42
jonatasmcampos <camposjonatas3@gmail.com>
2022-04-28 09:56:04 -0300

    atualizado

--- resources/views/usuarioAdmin/minhaConta/editSenha.blade.php
+++ resources/views/usuarioAdmin/minhaConta/editSenha.blade.php
@@ -1,22 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-externo">
-    
-    {{-- PARTE DO CONTEUDO --}}
-    <div class="container-conteudo">
-        
-        @include('navbar.navbar', ['titulo' => 'Editar Senha'])
-        
+    <div class="container-externo">
+
         @include('usuarioAdmin.minhaConta.inc._menus')
-     
+
         <div class="mx-5">
             <form id="update_senha" method="POST">
                 @csrf
                 @include('usuarioAdmin.minhaConta.inc._formSenha')
             </form>
-            </div>
-
         </div>
     </div>
 @endsection
