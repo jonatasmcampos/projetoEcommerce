a9db8f377f3bc824124753522d9cef47d7167156
josehs20 <josehenriquesimiquelsouza@gmail.com>
2022-06-09 19:44:47 -0300

    view finalizar compra iniciada

--- resources/views/usuario/carrinho/carrinho.blade.php
+++ resources/views/usuario/carrinho/carrinho.blade.php
@@ -15,111 +15,12 @@
             </div>
         </div>
     </div>
-
-
-    <div class="item-carrinho">
-        <img src="{{ asset('img/roupa1.png') }}" alt="produto">
-        <div class="item-carrinho-2">
-            <h5>Nome do produto</h5>
-            <h6><span>Cor:</span> &nbsp; CINZA</h6>
-            <h6><span>Tamanho:</span> &nbsp; G</h6>
-            <h6><span>Quantidade:</span> &nbsp; 2</h6>
-            <div class="carrinho-preco">
-                <h6><span>Unitário:</span> &nbsp; R$ 15,9</h6>
-                <h6><span>Total:</span> &nbsp; R$ 31,8</h6>
-            </div>
-        </div>
-    </div>
-
-    <div class="item-carrinho">
-        <img src="{{ asset('img/roupa1.png') }}" alt="produto">
-        <div class="item-carrinho-2">
-            <h5>Nome do produto</h5>
-            <h6><span>Cor:</span> &nbsp; CINZA</h6>
-            <h6><span>Tamanho:</span> &nbsp; G</h6>
-            <h6><span>Quantidade:</span> &nbsp; 2</h6>
-            <div class="carrinho-preco">
-                <h6><span>Unitário:</span> &nbsp; R$ 15,9</h6>
-                <h6><span>Total:</span> &nbsp; R$ 31,8</h6>
-            </div>
-        </div>
-    </div>
-
-    <div class="item-carrinho">
-        <img src="{{ asset('img/roupa1.png') }}" alt="produto">
-        <div class="item-carrinho-2">
-            <h5>Nome do produto</h5>
-            <h6><span>Cor:</span> &nbsp; CINZA</h6>
-            <h6><span>Tamanho:</span> &nbsp; G</h6>
-            <h6><span>Quantidade:</span> &nbsp; 2</h6>
-            <div class="carrinho-preco">
-                <h6><span>Unitário:</span> &nbsp; R$ 15,9</h6>
-                <h6><span>Total:</span> &nbsp; R$ 31,8</h6>
-            </div>
-        </div>
-    </div>
-
-    <div class="item-carrinho">
-        <img src="{{ asset('img/roupa1.png') }}" alt="produto">
-        <div class="item-carrinho-2">
-            <h5>Nome do produto</h5>
-            <h6><span>Cor:</span> &nbsp; CINZA</h6>
-            <h6><span>Tamanho:</span> &nbsp; G</h6>
-            <h6><span>Quantidade:</span> &nbsp; 2</h6>
-            <div class="carrinho-preco">
-                <h6><span>Unitário:</span> &nbsp; R$ 15,9</h6>
-                <h6><span>Total:</span> &nbsp; R$ 31,8</h6>
-            </div>
-        </div>
-    </div>
-
-    <div class="item-carrinho">
-        <img src="{{ asset('img/roupa1.png') }}" alt="produto">
-        <div class="item-carrinho-2">
-            <h5>Nome do produto</h5>
-            <h6><span>Cor:</span> &nbsp; CINZA</h6>
-            <h6><span>Tamanho:</span> &nbsp; G</h6>
-            <h6><span>Quantidade:</span> &nbsp; 2</h6>
-            <div class="carrinho-preco">
-                <h6><span>Unitário:</span> &nbsp; R$ 15,9</h6>
-                <h6><span>Total:</span> &nbsp; R$ 31,8</h6>
-            </div>
-        </div>
-    </div>
-
-    <div class="item-carrinho">
-        <img src="{{ asset('img/roupa1.png') }}" alt="produto">
-        <div class="item-carrinho-2">
-            <h5>Nome do produto</h5>
-            <h6><span>Cor:</span> &nbsp; CINZA</h6>
-            <h6><span>Tamanho:</span> &nbsp; G</h6>
-            <h6><span>Quantidade:</span> &nbsp; 2</h6>
-            <div class="carrinho-preco">
-                <h6><span>Unitário:</span> &nbsp; R$ 15,9</h6>
-                <h6><span>Total:</span> &nbsp; R$ 31,8</h6>
-            </div>
-        </div>
-    </div>
-
-    <div class="item-carrinho">
-        <img src="{{ asset('img/roupa1.png') }}" alt="produto">
-        <div class="item-carrinho-2">
-            <h5>Nome do produto</h5>
-            <h6><span>Cor:</span> &nbsp; CINZA</h6>
-            <h6><span>Tamanho:</span> &nbsp; G</h6>
-            <h6><span>Quantidade:</span> &nbsp; 2</h6>
-            <div class="carrinho-preco">
-                <h6><span>Unitário:</span> &nbsp; R$ 15,9</h6>
-                <h6><span>Total:</span> &nbsp; R$ 31,8</h6>
-            </div>
-        </div>
-    </div>
     <!-- endforeach -->
 
     <!-- BOTOES -->
     <div class="finalizar-compra">
         <a href="#continuarcomprando">Continuar comprando</a>
-        <button type="button" class="btn btn-primary">Finalizar compra</button>
+        <a href="{{route('finaliza-compra.index')}}" type="button" class="btn btn-primary">Finalizar compra</a>
     </div>
     <!-- FIM BOTOES -->
 </aside>
--- resources/views/usuario/welcome.blade.php
+++ resources/views/usuario/welcome.blade.php
@@ -6,12 +6,12 @@
     <!-- INCLUINDO A VIEW DE CARRINHO -->
     <form action="{{ route('usuariohome') }}" method="GET">
         @csrf
-    <!-- CONTEUDO PRINCIPAL DO SITE -->
-    <main class="conteudoPrincipal" style="padding: 0 25px">
+        <!-- CONTEUDO PRINCIPAL DO SITE -->
+        <main class="conteudoPrincipal" style="padding: 0 25px">
 
-        <!-- FILTROS -->
-        <aside class="filtros">
-          
+            <!-- FILTROS -->
+            <aside class="filtros">
+
                 <!-- CATEGORIAS -->
                 <div style="width: 100%">
 
@@ -29,11 +29,12 @@
                                 @foreach ($categorias as $cat)
                                     <li class="form-check">
                                         <input class="form-check-input" name="cat" type="checkbox"
-                                            value="{{ $cat->id }}" id="flexCheckDefault" {{in_array($cat->id, $boxes['cat']) ? 'checked' : ''}}>
+                                            value="{{ $cat->id }}" id="flexCheckDefault"
+                                            {{ in_array($cat->id, $boxes['cat']) ? 'checked' : '' }}>
                                         <label class="form-check-label" for="flexCheckDefault">
                                             {{ $cat->nome }}
                                         </label>
-                                    </li> 
+                                    </li>
                                     {{-- <a href="{{ route('usuariohome', $cat->id) }}">
                                     <li>{{ $cat->nome }}</li>
                                 </a> --}}
@@ -59,12 +60,12 @@
                                 @foreach ($cores as $cor)
                                     <li class="form-check">
                                         <input class="form-check-input" type="checkbox" value="{{ $cor->id }}"
-                                            name="cor[]" id="flexCheckDefault" {{in_array($cor->id, $boxes['cor']) ? 'checked' : ''}}>
+                                            name="cor[]" id="flexCheckDefault"
+                                            {{ in_array($cor->id, $boxes['cor']) ? 'checked' : '' }}>
                                         <label class="form-check-label" for="flexCheckDefault">
                                             {{ $cor->nome }}
                                         </label>
                                     </li>
-                        
                                 @endforeach
                             </ul>
                         </div>
@@ -87,7 +88,8 @@
                                 @foreach ($tamanhos as $t)
                                     <li class="form-check">
                                         <input class="form-check-input" type="checkbox" value="{{ $t->id }}"
-                                            name="tam[]" id="flexCheckDefault" {{in_array($t->id, $boxes['tam']) ? 'checked' : ''}}>
+                                            name="tam[]" id="flexCheckDefault"
+                                            {{ in_array($t->id, $boxes['tam']) ? 'checked' : '' }}>
                                         <label class="form-check-label" for="flexCheckDefault">
                                             {{ $t->nome }}
                                         </label>
@@ -101,148 +103,149 @@
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Filtrar</button>
-            
-        </aside>
 
-        <!-- CONTEUDO FILTRADO -->
-        <section class="conteudoFiltrado">
+            </aside>
 
-            <!-- BOTAO DO MODAL PRA EXIBIR FILTROS NO MOBILE -->
-            <button type="button" class="btn btn-primary btn-filtrar" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Filtros
-            </button>
-            <!-- FILTRO DE MAIOR PREÇO / MENOR PREÇO -->
-            <div class="filtroMais">
-                <select class="form-select" name="orderSelected" aria-label="Default select example">
-                    <option value="0">Relevância</option>
-                    <option value="1">Menor preço</option>
-                    <option value="2">Maior preço</option>
-                </select>
-            </div>
-       
-            <!-- FIM FILTRO DE MAIOR PREÇO / MENOR PREÇO -->
+            <!-- CONTEUDO FILTRADO -->
+            <section class="conteudoFiltrado">
+
+                <!-- BOTAO DO MODAL PRA EXIBIR FILTROS NO MOBILE -->
+                <button type="button" class="btn btn-primary btn-filtrar" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal">
+                    Filtros
+                </button>
+                <!-- FILTRO DE MAIOR PREÇO / MENOR PREÇO -->
+                <div class="filtroMais">
+                    <select class="form-select" name="orderSelected" aria-label="Default select example">
+                        <option value="0">Relevância</option>
+                        <option value="1">Menor preço</option>
+                        <option value="2">Maior preço</option>
+                    </select>
+                </div>
+
+                <!-- FIM FILTRO DE MAIOR PREÇO / MENOR PREÇO -->
 
 
-            <!-- EXIBIÇÃO DOS PRODUTOS -->
-            <div class="exibicaoProdutos">
+                <!-- EXIBIÇÃO DOS PRODUTOS -->
+                <div class="exibicaoProdutos">
 
-                @foreach ($produtos as $prod)
-                    <div class="card cardProduto containerImagem">
-                        <img src="{{ asset(!count($prod->imagens) ? '' : $prod->imagens[0]->nome) }}"
-                            class="card-img-top imagemProduto" alt="Imagem do produto">
+                    @foreach ($produtos as $prod)
+                        <div class="card cardProduto containerImagem">
+                            <img src="{{ asset(!count($prod->imagens) ? '' : $prod->imagens[0]->nome) }}"
+                                class="card-img-top imagemProduto" alt="Imagem do produto">
 
-                        <div class="middle">
-                            <div class="btnVerProduto">
-                                <a href="{{ route('produto.show', $prod->id) }}">Ver detalhes</a>
+                            <div class="middle">
+                                <div class="btnVerProduto">
+                                    <a href="{{ route('produto.show', $prod->id) }}">Ver detalhes</a>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    {{ $prod->nome }}
+                                </h5>
+                                <div class="card-text precoProduto">
+                                    {{ $prod->preco }}
+                                </div>
                             </div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                {{ $prod->nome }}
-                            </h5>
-                            <div class="card-text precoProduto">
-                                {{ $prod->preco }}
+                    @endforeach
+
+                </div>
+                <!-- FIM EXIBIÇÃO DOS PRODUTOS -->
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-titl text-center" id="exampleModalLabel">Filtros</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                             </div>
-                        </div>
-                    </div>
-                @endforeach
+                            <div class="modal-body">
+                                <!-- FILTROS -->
+                                <aside class="filtros" style="display: block; width: 100%">
+                                    <h5 style="margin-left: 35%; margin-top: 10px; margin-bottom: 10px">Filtros</h5>
+                                    <hr>
+                                    <!-- CATEGORIAS -->
+                                    <div style="width: 100%">
 
-            </div>
-            <!-- FIM EXIBIÇÃO DOS PRODUTOS -->
+                                        <a style="width: 100%; margin-bottom: 7px" class="btn btn-primary filtrar-por"
+                                            data-bs-toggle="collapse" href="#collapseExample" role="button"
+                                            aria-expanded="false" aria-controls="collapseExample">
+                                            <i class="fa fa-chevron-down" aria-hidden="true"></i> &nbsp; Categorias
+                                        </a>
 
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-titl text-center" id="exampleModalLabel">Filtros</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <!-- FILTROS -->
-                            <aside class="filtros" style="display: block; width: 100%">
-                                <h5 style="margin-left: 35%; margin-top: 10px; margin-bottom: 10px">Filtros</h5>
-                                <hr>
-                                <!-- CATEGORIAS -->
-                                <div style="width: 100%">
+                                        <div class="collapse show" id="collapseExample">
+                                            <div class="card card-body">
+                                                <ul class="list-group">
 
-                                    <a style="width: 100%; margin-bottom: 7px" class="btn btn-primary filtrar-por"
-                                        data-bs-toggle="collapse" href="#collapseExample" role="button"
-                                        aria-expanded="false" aria-controls="collapseExample">
-                                        <i class="fa fa-chevron-down" aria-hidden="true"></i> &nbsp; Categorias
-                                    </a>
-
-                                    <div class="collapse show" id="collapseExample">
-                                        <div class="card card-body">
-                                            <ul class="list-group">
-                                                
-                                                @foreach ($categorias as $cat)
-                                                    <a href="{{ route('usuariohome', $cat->id) }}"
-                                                        class="list-group-item">
-                                                        <li>{{ $cat->nome }}</li>
-                                                    </a>
-                                                @endforeach
-                                            </ul>
+                                                    @foreach ($categorias as $cat)
+                                                        <a href="{{ route('usuariohome', $cat->id) }}"
+                                                            class="list-group-item">
+                                                            <li>{{ $cat->nome }}</li>
+                                                        </a>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <!-- CORES -->
-                                <div style="width: 100%">
-                                    <a style="width: 100%; margin-bottom: 7px" class="btn btn-primary filtrar-por"
-                                        data-bs-toggle="collapse" href="#collapseExample1" role="button"
-                                        aria-expanded="false" aria-controls="collapseExample">
-                                        <i class="fa fa-chevron-down" aria-hidden="true"></i> &nbsp; Cores
-                                    </a>
+                                    <!-- CORES -->
+                                    <div style="width: 100%">
+                                        <a style="width: 100%; margin-bottom: 7px" class="btn btn-primary filtrar-por"
+                                            data-bs-toggle="collapse" href="#collapseExample1" role="button"
+                                            aria-expanded="false" aria-controls="collapseExample">
+                                            <i class="fa fa-chevron-down" aria-hidden="true"></i> &nbsp; Cores
+                                        </a>
 
-                                    <div class="collapse show" id="collapseExample1">
-                                        <div class="card card-body">
-                                            <ul class="list-group">
-                                                @foreach ($cores as $cor)
-                                                    <a href="{{ route('usuariohome', $cat->id) }}"
-                                                        class="list-group-item">
-                                                        <li>{{ $cor->nome }}</li>
-                                                    </a>
-                                                @endforeach
-                                            </ul>
+                                        <div class="collapse show" id="collapseExample1">
+                                            <div class="card card-body">
+                                                <ul class="list-group">
+                                                    @foreach ($cores as $cor)
+                                                        <a href="{{ route('usuariohome', $cat->id) }}"
+                                                            class="list-group-item">
+                                                            <li>{{ $cor->nome }}</li>
+                                                        </a>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <!-- TAMANHOS -->
-                                <div style="width: 100%">
-                                    <a style="width: 100%; margin-bottom: 7px" class="btn btn-primary filtrar-por"
-                                        data-bs-toggle="collapse" href="#collapseExample2" role="button"
-                                        aria-expanded="false" aria-controls="collapseExample">
-                                        <i class="fa fa-chevron-down" aria-hidden="true"></i> &nbsp; Tamanhos
-                                    </a>
+                                    <!-- TAMANHOS -->
+                                    <div style="width: 100%">
+                                        <a style="width: 100%; margin-bottom: 7px" class="btn btn-primary filtrar-por"
+                                            data-bs-toggle="collapse" href="#collapseExample2" role="button"
+                                            aria-expanded="false" aria-controls="collapseExample">
+                                            <i class="fa fa-chevron-down" aria-hidden="true"></i> &nbsp; Tamanhos
+                                        </a>
 
-                                    <div class="collapse show" id="collapseExample2">
-                                        <div class="card card-body">
-                                            <ul class="list-group">
-                                                @foreach ($tamanhos as $t)
-                                                    <a href="{{ route('usuariohome', $cat->id) }}"
-                                                        class="list-group-item">
-                                                        <li>{{ $t->nome }}</li>
-                                                    </a>
-                                                @endforeach
-                                            </ul>
+                                        <div class="collapse show" id="collapseExample2">
+                                            <div class="card card-body">
+                                                <ul class="list-group">
+                                                    @foreach ($tamanhos as $t)
+                                                        <a href="{{ route('usuariohome', $cat->id) }}"
+                                                            class="list-group-item">
+                                                            <li>{{ $t->nome }}</li>
+                                                        </a>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </aside>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">Filtrar </button>
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                </aside>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary">Filtrar </button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
     </form>
-        <!-- FIM CONTEUDO FILTRADO -->
+    <!-- FIM CONTEUDO FILTRADO -->
     </main>
 @endsection
